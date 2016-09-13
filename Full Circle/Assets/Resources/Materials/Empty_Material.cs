e7677426dcb63ef44683e02729d43fbc1552f0c9
Jack Gillette <jackdgillette@gmail.com>
2016-09-13 17:20:57 -0400

    Initial Project Push

---
+++ Full Circle/Assets/Resources/Materials/Empty_Material.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Empty_Material : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
